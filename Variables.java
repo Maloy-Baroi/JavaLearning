cde10ce76b16bf95948312a057b73022dd634c77
Maloy-Baroi <maloy.virat@gmail.com>
2023-03-23 09:47:23 +0600

    Different Data Type: Primitive

--- Variables.java
+++ Variables.java
@@ -1,6 +1,15 @@
 public class Variables {
     public static void main(String[] args) {
+//        Primitive variable
         int varInt = 10;
+        float varFloat = 10.5f;
+        byte varByte = 127;
+        char varChar = 'c'; // In Java single quotation means character and double quotation means string
+        boolean bool = true;
         System.out.println(varInt);
+        System.out.println(varFloat);
+        System.out.println(varByte);
+        System.out.println(varChar);
+        System.out.println(bool);
     }
 }
